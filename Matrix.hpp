0c8f31eaa7e8bc4888ad4499460cbe94c3c6f0b5
yuki12 <yuki12@users.noreply.github.com>
2018-10-04 00:14:43 +0900

    アクセス用テンプレートを使って整理

--- Matrix.hpp
+++ Matrix.hpp
@@ -5,23 +5,44 @@ template <typename T, size_t R, size_t C> using Matrix = T[R][C];
 template <typename T, size_t L> using Vector = Matrix<T, L, 1>;
 
 template <typename T, size_t R, size_t C>
-void setIdentity (Matrix<T, R, C> &dst)
+constexpr T at (const Matrix<T, R, C> &src, size_t i, size_t j)
 {
-    for (size_t i = 0; i < R; ++i)
-        for (size_t j = 0; j < C; ++j)
-            dst[i][j] = i == j ? 1 : 0;
+    return src[i][j];
 }
 template <typename T, size_t R, size_t C>
-void setScalar (Matrix<T, R, C> &dst, T value)
+constexpr std::size_t rows (const Matrix<T, R, C> &)
 {
-    for (size_t i = 0; i < R; ++i)
-        for (size_t j = 0; j < C; ++j)
-            dst[i][j] = value;
+    return R;
 }
-template <typename T, size_t R, size_t C> void setZero (Matrix<T, R, C> &dst)
+template <typename T, size_t R, size_t C>
+constexpr std::size_t columns (const Matrix<T, R, C> &)
 {
-    for (size_t i = 0; i < R; ++i)
-        for (size_t j = 0; j < C; ++j)
-            dst[i][j] = 0;
+    return C;
 }
+
+template <typename T, size_t R, size_t C>
+constexpr T &ref (Matrix<T, R, C> &src, size_t i, size_t j)
+{
+    return src[i][j];
+}
+
+template <typename T> void setIdentity (T &dst)
+{
+    for (size_t i = 0; i < rows (dst); ++i)
+        for (size_t j = 0; j < columns (dst); ++j)
+            ref (dst, i, j) = i == j ? 1 : 0;
+}
+template <typename T, typename U> void setScalar (T &dst, U value)
+{
+    for (size_t i = 0; i < rows (dst); ++i)
+        for (size_t j = 0; j < columns (dst); ++j)
+            ref (dst, i, j) = value;
+}
+template <typename T> void setZero (T &dst)
+{
+    for (size_t i = 0; i < rows (dst); ++i)
+        for (size_t j = 0; j < columns (dst); ++j)
+            ref (dst, i, j) = 0;
+}
+
 } // namespace numcpp
--- test.cpp
+++ test.cpp
@@ -2,5 +2,7 @@
 int main ()
 {
     numcpp::Matrix<double, 3, 4> m;
+    numcpp::setIdentity(m);
+    numcpp::setZero(m);
     return 0;
 }
